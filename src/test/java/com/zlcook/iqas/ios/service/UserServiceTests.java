cd970ae448b0e343c1ba212bca1687494cf4463c
zhougit <594389970@qq.com>
2017-03-02 09:56:04 +0800

    修复了测试代码

--- src/test/java/com/zlcook/iqas/ios/service/UserServiceTests.java
+++ src/test/java/com/zlcook/iqas/ios/service/UserServiceTests.java
@@ -1,12 +1,18 @@
 package com.zlcook.iqas.ios.service;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.zlcook.iqas.ios.form.RegisterForm;
 import com.zlcook.iqas.ios.model.User;
+
 
 /**
 * @author 周亮 
@@ -17,6 +23,7 @@ import com.zlcook.iqas.ios.model.User;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceTests {
+	private Logger logger = LogManager.getLogger(UserServiceTests.class);
 	@Autowired
 	private UserService userService;
 	@Test
@@ -31,6 +38,16 @@ public class UserServiceTests {
 		user.setLoginName("liang");
 		user.setRealName("周亮");
 		user.setPassword("123");
+		RegisterForm form =new RegisterForm();
+		BeanUtils.copyProperties(user, form);
+		int regResult =userService.register(form);
+		if( regResult ==1)
+		{
+			logger.info("注册成功："+user.getLoginName());
+			return ;
+		}
+
+		logger.info("用户已存在："+user.getLoginName());
 	}
 
 	public UserService getUserService() {
